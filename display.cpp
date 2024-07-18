73a6f55e8d60087835d0d4169a7cd229f36a4a9e
bishal <bishal777giri@gmail.com>
2024-07-18 21:40:04 +0545

    second commit

--- display.cpp
+++ display.cpp
@@ -1,1 +1,21 @@
 //This program takes info of student and display it on the screen
+#include<iostream>
+using namespace std;
+
+class Student{
+    char name[20];
+    int roll;
+    int address;
+    int marks;
+
+    public:
+    void getdata();
+    void displaydata();
+};
+
+void Student::getdata(){
+
+}
+void Student::displaydata(){
+
+}
