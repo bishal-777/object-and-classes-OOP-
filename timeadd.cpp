5f0f6b4e0e8c20fd87fae39ef005d5b7a0214fb9
bishal <bishal777giri@gmail.com>
2024-07-21 18:19:55 +0545

    second commit

--- timeadd.cpp
+++ timeadd.cpp
@@ -13,14 +13,29 @@ class Time{
 };
 
 void Time :: get_time(){
-
+    cout<<"Enter time in hr::min::sec\t";
+    cin>>hr>>min>>sec;
 }
 
 void Time :: display_time(){
-    
+    cout<<"The added time is:\t"<<hr<<":"<<min<<":"<<sec<<"\n";
 }
 
-Time Time :: add_time(Time t){
-
+Time Time :: add_time(Time t2){
+    Time t;
+    t.sec = sec + t2.sec;
+    t.min = min + t2.min + t.sec / 60;
+    t.sec = t.sec % 60;
+    t.hr = hr + t2.hr + t.min / 60;
+    t.min = t.min % 60;
+    return t;
 };
-    
+
+int main(){
+    Time t1,t2,t3;
+    t1.get_time();
+    t2.get_time();
+    t3=t1.add_time(t2);
+    t3.display_time();
+    return 0;
+}
